ff702c0eaa4d56bcc528bcb05645f5f9811c32ee
Mehmed Jusic <mehmed.jusic@bpuholdings.com>
2019-03-03 23:52:04 +0100

    User roles/groups

--- src/containers/DefaultLayout/DefaultHeader.js
+++ src/containers/DefaultLayout/DefaultHeader.js
@@ -1,43 +1,39 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { Badge, DropdownItem, DropdownMenu, DropdownToggle, Nav, NavItem, NavLink } from 'reactstrap';
+import React, {Component} from 'react';
+import {DropdownToggle, Nav} from 'reactstrap';
 import PropTypes from 'prop-types';
 
-import { AppAsideToggler, AppHeaderDropdown, AppNavbarBrand, AppSidebarToggler } from '@coreui/react';
+import {AppHeaderDropdown, AppNavbarBrand, AppSidebarToggler} from '@coreui/react';
 import bpu from '../../assets/img/brand/bpu.png'
-import sygnet from '../../assets/img/brand/sygnet.svg'
 
 const propTypes = {
-  children: PropTypes.node,
+    children: PropTypes.node,
 };
 
 const defaultProps = {};
 
 class DefaultHeader extends Component {
-  render() {
+    render() {
 
-    // eslint-disable-next-line
-    const { children, ...attributes } = this.props;
+        return (
+            <React.Fragment>
+                <AppSidebarToggler className="d-lg-none" display="md" mobile/>
+                <AppNavbarBrand
+                    full={{src: bpu, width: 68, height: 50, alt: 'BPU Logo'}}
+                    minimized={{src: bpu, width: 30, height: 30, alt: 'BPU Logo'}}
+                />
+                <AppSidebarToggler className="d-md-down-none" display="lg"/>
 
-    return (
-      <React.Fragment>
-        <AppSidebarToggler className="d-lg-none" display="md" mobile />
-        <AppNavbarBrand
-          full={{ src: bpu, width: 68, height: 50, alt: 'BPU Logo' }}
-          minimized={{ src: sygnet, width: 30, height: 30, alt: 'BPU Logo' }}
-        />
-        <AppSidebarToggler className="d-md-down-none" display="lg" />
-
-        <Nav className="ml-auto" navbar>
-          <AppHeaderDropdown direction="down">
-            <DropdownToggle nav className="m-2">
-              <img src={'../../assets/img/avatars/6.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com" />
-            </DropdownToggle>
-          </AppHeaderDropdown>
-        </Nav>
-      </React.Fragment>
-    );
-  }
+                <Nav className="ml-auto" navbar>
+                    <AppHeaderDropdown direction="down">
+                        <DropdownToggle nav className="m-2">
+                            <img src={'../../assets/img/avatars/6.jpg'} className="img-avatar"
+                                 alt="admin@bootstrapmaster.com"/>
+                        </DropdownToggle>
+                    </AppHeaderDropdown>
+                </Nav>
+            </React.Fragment>
+        );
+    }
 }
 
 DefaultHeader.propTypes = propTypes;
